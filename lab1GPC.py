c83e7b8f242d8b231d4f6762f39fb48ebaaaf167
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2024-07-17 18:59:22 -0600

    resto de poligonos

--- lab1GPC.py
+++ lab1GPC.py
@@ -9,6 +9,14 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Rellenar Polígono")
 
 poligono1 = [(165, 380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)]
+poligono2 = [(321, 335), (288, 286), (339, 251), (374, 302)]
+poligono3 = [(377, 249), (411, 197), (436, 249)]
+poligono4 = [(413, 177), (448, 159), (502, 88), (553, 53), (535, 36), (676, 37), (660, 52),
+(750, 145), (761, 179), (672, 192), (659, 214), (615, 214), (632, 230), (580, 230),
+(597, 215), (552, 214), (517, 144), (466, 180)]
+poligono5 = [(682, 175), (708, 120), (735, 148), (739, 170)]
+
+
 
 # Función para dibujar líneas 
 def poligono_lineas(screen, points):
@@ -53,7 +61,14 @@ while running:
     
     poligono_lineas(screen, poligono1)
     poligono_llenar(screen, poligono1)
-    
+    poligono_lineas(screen, poligono2)
+    poligono_llenar(screen, poligono2)
+    poligono_lineas(screen, poligono3)
+    poligono_llenar(screen, poligono3)
+    poligono_lineas(screen, poligono4)
+    poligono_llenar(screen, poligono4)
+    poligono_lineas(screen, poligono5)
+    poligono_llenar(screen, poligono5)
     # Actualizar la pantalla
     pygame.display.flip()
 
